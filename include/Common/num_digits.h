c57db4adebb6d52e5d3c5f3c6413a52b4f2bc1c1
Dustin Biser <dustinbiser@gmail.com>
2012-01-10 01:21:05 -0500

    Removed all references to num_digits.cpp and num_digits.h

--- include/Common/num_digits.h
+++
@@ -1,9 +0,0 @@
-#ifndef NUM_DIGITS_H
-#define NUM_DIGITS_H
-#include <cmath>
-
-namespace Rigid2D {
-  // Returns the number of digits of value.
-  extern unsigned int numberOfDigits (long value);
-}
-#endif
