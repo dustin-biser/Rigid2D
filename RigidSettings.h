1ef457b28c4795d9fa4907a954e57010c220f334
Dustin Biser <dustinbiser@gmail.com>
2012-01-10 19:59:31 -0500

    Added #include <cassert> to RigidSettings.h

--- RigidSettings.h
+++ RigidSettings.h
@@ -1,6 +1,10 @@
 #ifndef RIGID_SETTINGS_H
 #define RIGID_SETTINGS_H
 
+// Uncomment next line to compile out asserts from all source files.
+//#define NDEBUG
+#include <cassert>
+
 // Place all global library settings and constants in this file.
 namespace Rigid2D{
 	
