5c181adc38df8d8d2a94fcc4189b9837a7f5769d
Dustin Biser <dustinbiser@gmail.com>
2012-01-14 19:31:31 -0500

    Changed float to Real in Vector3.h

--- include/Common/Vector3.h
+++ include/Common/Vector3.h
@@ -1,26 +1,27 @@
 #ifndef RIGID_VECTOR3_H
 #define RIGID_VECTOR3_H
+#include "RigidSettings"
 #include <cassert>
 #include <cmath>
 #include "feq.h"
 
 namespace Rigid2D {
 
-  /* Standard vector for 3D floats */
+  /* Standard vector for 3D Reals */
   class Vector3 {
     public:
-      float x, y, z;
+      Real x, y, z;
 
     public:
       Vector3() {}
 
-      Vector3(const float X, const float Y, const float Z) {
+      Vector3(const Real X, const Real Y, const Real Z) {
         x = X;
         y = Y;
         z = Z;
       }
 
-      Vector3(const float arr[3]) {
+      Vector3(const Real arr[3]) {
         x = arr[0];
         y = arr[1];
         z = arr[2];
@@ -45,30 +46,30 @@ namespace Rigid2D {
       friend Vector3 operator-(const Vector3 & l, const Vector3 & r) {
         return Vector3(l.x - r.x, l.y - r.y, l.z - r.z);
       }
-      
+
       void operator+=(const Vector3 & vector) {
         x += vector.x;
         y += vector.y;
         z += vector.z;
       }
-      
+
       void operator-=(const Vector3 & vector) {
         x -= vector.x;
         y -= vector.y;
         z -= vector.z;
       }
 
-      void operator*=(float scalar) {
+      void operator*=(Real scalar) {
         x *= scalar;
         y *= scalar;
         z *= scalar;
       }
 
-      Vector3 operator*(float scalar) {
+      Vector3 operator*(Real scalar) {
         return Vector3(x * scalar, y * scalar, z * scalar);
       }
- 
-      void operator/=(float scalar) {
+
+      void operator/=(Real scalar) {
         assert(scalar != 0);
         x /= scalar;
         y /= scalar;
@@ -79,20 +80,20 @@ namespace Rigid2D {
         return (feq(x, vec.x) && feq(y, vec.y) && feq(z, vec.z));
       }
 
-      float operator[](const unsigned int i) const {
+      Real operator[](const unsigned int i) const {
         assert(i < 3);
         return *(&x + i);
       }
 
-      float getLengthSquared() const {
+      Real getLengthSquared() const {
         return x * x + y * y + z * z;
       }
 
-      float getLength() const {
+      Real getLength() const {
         return sqrt(x * x + y * y + z * z);
       }
 
-      float dot(const Vector3 & vec) const {
+      Real dot(const Vector3 & vec) const {
         return x * vec.x + y * vec.y + z * vec.z;
       }
 
@@ -105,14 +106,11 @@ namespace Rigid2D {
       }
 
       void normalize() {
-        float length = getLength();
+        Real length = getLength();
         x /= length;
         y /= length;
         z /= length;
       }
-
-
   };
-
 }
 #endif
