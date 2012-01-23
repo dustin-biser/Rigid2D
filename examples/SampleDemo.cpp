7e4f6361d15bba4fcae54779c7586860544b66f6
Michail Denchev <mdenchev@gmail.com>
2012-01-23 16:22:26 -0500

    Got basic rendering working

--- examples/SampleDemo.cpp
+++ examples/SampleDemo.cpp
@@ -1,13 +1,14 @@
 #include "SampleDemo.h"
+#include <QMouseEvent>
 
 SampleDemo::SampleDemo(QWidget *parent)
         : QGLWidget(parent) 
 {
   setMouseTracking(true);
-
-  std::cout << "t";
+  setAutoBufferSwap(true);
+ 
   animationTimer = new QTimer(this);
-  connect(animationTimer, SIGNAL(timeout()), this, SLOT(paintGL()));
+  connect(animationTimer, SIGNAL(timeout()), this, SLOT(updateGL()));
   animationTimer->start(0);
 
   fpsTimer = new QTime;
@@ -21,18 +22,28 @@ SampleDemo::~SampleDemo()
 
 void SampleDemo::initializeGL()
 {
-  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glClearDepth(1.0f);
+
   glEnable(GL_DEPTH_TEST);
 }
 
 void SampleDemo::resizeGL(int w, int h)
 {
-  glViewport(0, 0, (GLint) w, (GLint)h);
+  h = h?h:1;
+
+  glViewport( 0, 0, (GLint)w, (GLint)h );
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(45.0f,(GLfloat)w / (GLfloat)h, 0.1f, 1000.0f);
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
 }
 
 void SampleDemo::paintGL()
 {
-  ///std::cout << "T";
   // Figure out fps
   float elapsed = fpsTimer->elapsed();
   if (elapsed >= 1000) {
@@ -44,24 +55,25 @@ void SampleDemo::paintGL()
   frameCount++;
 
   // Do drawing here!!
-  glClear (GL_COLOR_BUFFER_BIT);
-  glClearColor(1.0, 0.0, 0.0, 0.0);
+  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glLoadIdentity();
+
+  glTranslatef(0, 0, -300);
 
   glBegin (GL_TRIANGLES);
-  glColor3f (1.0, 0.0, 0.0);
-  glVertex2f (5.0, 5.0);
-  glColor3f (0.0, 1.0, 0.0);
-  glVertex2f (25.0, 5.0);
-  glColor3f (0.0, 0.0, 1.0);
-  glVertex2f (5.0, 25.0);
+  glColor3f (1, 0, 0);
+  glVertex2f (0, 0);
+  glColor3f (0, 1, 0);
+  glVertex2f (30, 0);
+  glColor3f (0, 0, 1);
+  glVertex2f (15, 15);
   glEnd();
 
-  glFlush();
 }
 
 void SampleDemo::mousePressEvent(QMouseEvent *event) 
 {
-
+  std::cout << event->y() << std::endl;
 }
 
 void SampleDemo::mouseMoveEvent(QMouseEvent *event) 
