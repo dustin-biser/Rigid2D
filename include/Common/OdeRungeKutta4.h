3aee50f6995b523665f3ede1f3723fe0da2a31f4
Dustin Biser <dustinbiser@gmail.com>
2012-01-30 22:39:37 -0500

    Removed vectors.  Sticking with pointers which are orders of magnitude faster for passing to functions.

--- include/Common/OdeRungeKutta4.h
+++ include/Common/OdeRungeKutta4.h
@@ -3,27 +3,33 @@
 #include "RigidSettings.h"
 #include "OdeSolver.h"
 #include "RigidException.h"
+#include <vector>
+using std::vector;
 
 namespace Rigid2D{
   // Implementation of a 4th order 4 stage Runge Kutta method for approximating
   // solutions to the system dx/dt = f(t,x), where x and f are m-dimensional vectors.
   class OdeRungeKutta4 : public OdeSolver {
     public:
-      OdeRungeKutta4(unsigned int dimension, Real stepSize, Function function, void* optionalData = 0);
+      OdeRungeKutta4(unsigned int dimension, Real step, DerivFunction dxdt, void* optionalData = 0);
 
-      ~OdeRungeKutta4();
+      ~OdeRungeKutta4() { }
 
-      // Advances time t and state vector x to the next step of approximate
-      // solution to dx/dt = f(t,x).
-      virtual void ProcessNextStep (Real& t, Real* x);
+      // Advances tOut and xOut by one step from tIn and xIn, respectively
+      virtual void processNextStep (Real tIn, Real* xIn, Real& tOut, Real* xOut);
+
+      // A simplier way to call processNextStep
+      virtual void processNextStep (Real t, Real* x);
+
+      virtual void setDimension (unsigned int newDim);
     private:
       Real oneHalfStep_;
       Real oneSixthStep_;
-      Real * kn1_;
-      Real * kn2_;
-      Real * kn3_;
-      Real * kn4_;
-      Real * tmp_;
+      vector<Real> kn1_;
+      vector<Real> kn2_;
+      vector<Real> kn3_;
+      vector<Real> kn4_;
+      vector<Real> tmp_;
   };
 }
 #endif
