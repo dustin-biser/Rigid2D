2e5fb7e62f24ac3fb5845b9afb7c249b4bb69e68
Michail Denchev <mdenchev@gmail.com>
2012-01-30 23:50:17 -0500

    modified vector test

--- SpeedTests/return_vector.cpp
+++ SpeedTests/return_vector.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 #define COUNT 10000
 
-vector<int> foo (vector<int> v){
-  for(int i; i < COUNT; ++i){
+vector<int> & foo (vector<int> & v){
+  for(int i = 0; i < COUNT; ++i){
     v[i] += 1;
   }
   return v;
 }
 
 float * foo (float * f){
-  for(int i; i < COUNT; ++i){
+  for(int i = 0; i < COUNT; ++i){
     f[i] += 1;
   }
 
@@ -30,39 +30,39 @@ void foo (vector<int> vIn, vector<int> vOut){
 int main(){
   Timer timer;
   vector<int> v(COUNT, 0);
-  vector<int> tmpv(COUNT, 0);
+  vector<int> tmpv;
   float *p = new float [COUNT];
   float *tmpf;
   int i;
 
   //fill in p
   for(i = 0; i < COUNT; ++i){
-    p[i] = i;
+    p[i] = 0;
   }
 
-  timer.start();
   cout << "Pointer return: ";
+  timer.start();
   for(i = 0; i < COUNT; ++i){
     tmpf = foo(p);
   }
   timer.stop();
   cout << timer.getElapsedTimeInMilliSec() << " ms.\n";
 
-  timer.start();
   cout << "Vector return: ";
+  timer.start();
   for(i = 0; i < COUNT; ++i){
     tmpv = foo(v);
   }
   timer.stop();
   cout << timer.getElapsedTimeInMilliSec() << " ms.\n";
 
-  timer.start();
-  cout << "Void return: ";
-  for(i = 0; i < COUNT; ++i){
-    foo(v, tmpv);
-  }
-  timer.stop();
-  cout << timer.getElapsedTimeInMilliSec() << " ms.\n";
+  //cout << "Void return: ";
+  //timer.start();
+  //for(i = 0; i < COUNT; ++i){
+    //foo(v, tmpv);
+  //}
+  //timer.stop();
+  //cout << timer.getElapsedTimeInMilliSec() << " ms.\n";
 
   delete [] p;
 
