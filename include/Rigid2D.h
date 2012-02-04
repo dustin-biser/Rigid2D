72ebffd28f848a3617c9f1e6a7e2f4a9f4a9323d
Michail Denchev <mdenchev@gmail.com>
2012-02-04 14:10:12 -0500

    Added Rigid2D.h which contains all header files in Rigid2D/include. Should be updated when new headers are made. Should also not contain any internals only header files.

---
+++ include/Rigid2D.h
@@ -0,0 +1,15 @@
+#ifndef RIGID2D_H
+#define RIGID2D_H
+
+#include "Common/MathUtils.h"
+#include "Common/feq.h"
+#include "Common/Matrix2.h"
+#include "Common/OdeSolver.h" // -
+#include "Common/OdeRungeKutta4.h" // prob don't need both
+#include "Common/RigidException.h"
+#include "Common/Vector2.h"
+#include "Common/Vector3.h"
+
+#include "Objects/RigidBody.h"
+
+#endif
