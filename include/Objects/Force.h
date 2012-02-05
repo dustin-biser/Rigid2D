27a3a90b5a70c8e3c60079d54702bf86a1bdcf03
Dustin Biser <dustinbiser@gmail.com>
2012-02-05 02:00:42 -0500

    Created Force.h

---
+++ include/Objects/Force.h
@@ -0,0 +1,34 @@
+#ifndef RIGID2D_FORCE_H
+#define RIGID2D_FORCE_H
+
+namespace Rigid2D{
+  class Force {
+    public:
+      typedef
+      void (*ForceFunction)(RigidBodies * rigidBodies,   // Collection of RigidBodies
+                                                         // that the force is acting upon.
+
+                            Real * dst,                  // Destination for storing force
+                                                         // vectors.
+
+                            void * optionalData);        // User specific data which can be
+                                                         // used in computing forces.
+      Force();
+      ~Force();
+      void applyForce();
+      bool addRigidBodies(RigidBody * rigidBodyArray);
+      bool removeRigidBodies(RigidBody * rigidBodyArray);
+      bool isEnabled();
+      void setEnabled();
+
+    protected:
+      Vector2 * forceVectors_
+      RigidBody * rigidBodies_
+      unsigned int numBodies_
+      void * optionalData_
+      bool enabled_
+      ForceFunction f_
+  };
+} // end namespace Rigid2D
+
+#endif
