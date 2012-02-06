abe3a65633855fe00aad0562a82fa5d1c522d1b7
Dustin Biser <dustinbiser@gmail.com>
2012-02-06 12:21:43 -0500

    Commented most things for Force.h.  Change optionalData to userData

---
+++ include/Common/Tests/Force.cpp
@@ -0,0 +1,11 @@
+#include "Force.h"
+
+using namespace Rigid2D;
+
+Force::Force(ForceFunction funct, RigidBody * rigidBodyArray = 0, void * userData = 0){
+
+}
+
+Force::Force(ForceFunction funct, void * userData = 0){
+
+}
