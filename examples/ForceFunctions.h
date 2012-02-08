789e9902c51d8924676fcb9d783bf31e931b1586
Dustin Biser <dustinbiser@gmail.com>
2012-02-07 22:24:24 -0500

    Wrote mouseSpringForce function

---
+++ examples/ForceFunctions.h
@@ -0,0 +1,9 @@
+#ifndef FORCE_FUNCTIONS_H
+#define FORCE_FUNCTIONS_H
+#include "include/Rigid2D.h"
+
+using namespace Rigid2D;
+
+void mouseSpringForce(RigidBody * rigidBody, Vector2 * dst, void * userData);
+
+#endif
