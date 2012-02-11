ffb966996fe034c9b264b602f06c09158efb9db5
Michail Denchev <mdenchev@gmail.com>
2012-02-11 12:30:41 -0500

    Added additional documentation to RigidBodySystem

--- include/Objects/RigidBodySystem.cpp
+++ include/Objects/RigidBodySystem.cpp
@@ -3,75 +3,72 @@
 #include "../Common/RigidException.h"
 #include <new>
 
+using namespace std;
 namespace Rigid2D
 {
-	RigidBodySystem::RigidBodySystem( )
-	{
-		time_ = 0.0;
+  RigidBodySystem::RigidBodySystem( )
+  {
+    time_ = 0.0;
 
-		try {
-			// solver_ = new RungeKutta4RigidBodySolver(0,                 // dimension of system
-			//     StartingStepSize,  // stepSize
-			//     0,                 // null pointer to DerivFunc
+    try {
+      // solver_ = new RungeKutta4RigidBodySolver(0,                 // dimension of system
+      //     StartingStepSize,  // stepSize
+      //     0,                 // null pointer to DerivFunc
 
-			//     this); // pass self to optionalData
-			// pointer so that OdeSolver
-			// can access RigidBodySystem
-			// state information
-		}
-		catch (std::bad_alloc error){
-			//throw InternalErrorException(__LINE__, __FUNCTION__, __FILE__,
-			//  "Memory Allocation Failure");
-		}
+      //     this); // pass self to optionalData
+      // pointer so that OdeSolver
+      // can access RigidBodySystem
+      // state information
+    }
+    catch (std::bad_alloc error){
+      //throw InternalErrorException(__LINE__, __FUNCTION__, __FILE__,
+      //  "Memory Allocation Failure");
+    }
 
-	}
+  }
 
-	RigidBodySystem::~RigidBodySystem()
-	{
-		//delete solver_;
-	}
+  RigidBodySystem::~RigidBodySystem()
+  {
+    //delete solver_;
+  }
 
-	void RigidBodySystem::updateRigidBodies()
-	{
-		// Build state array S_ and zero the forceAccumulator for each RigidBody
-		unsigned int i;
-		for(i = 0; i < numBodies_; i++){
+  void RigidBodySystem::updateRigidBodies()
+  {
+    // Build state array S_ and zero the forceAccumulator for each RigidBody
 
-		}
+  }
 
-	}
+  void RigidBodySystem::addRigidBody(RigidBody *rigidBody)
+  {
 
-	void RigidBodySystem::addRigidBody(RigidBody *rigidBody)
-	{
+  }
 
-	}
+  void RigidBodySystem::addRigidBodies(RigidBody * rigidBodyArray, unsigned int numBodies)
+  {
 
-	void RigidBodySystem::addRigidBodies(RigidBody * rigidBodyArray, unsigned int numBodies)
-	{
+  }
 
-	}
+  void RigidBodySystem::removeRigidBody(RigidBody *rigidBody)
+  {
 
-	void RigidBodySystem::removeRigidBody(RigidBody *rigidBody)
-	{
+  }
 
-	}
+  void RigidBodySystem::removeRigidBodies(RigidBody * rigidBodyArray, unsigned int numBodies)
+  {
 
-	void RigidBodySystem::removeRigidBodies(RigidBody * rigidBodyArray, unsigned int numBodies)
-	{
-
-	}
+  }
 
 
 
-	void RigidBodySystem::buildSystemStateArray(){
-		int i;
+  void RigidBodySystem::buildSystemStateArray(){
+    // Iterate through the unordered_set rigidBodies_
+    unordered_set<RigidBody*>::iterator it;
 
-		for(i = 1; i <= numBodies_; ++i){
-			//*S_++ = rigidBodies_->getPosition();
+    for(it = rigidBodies_.begin(); it != rigidBodies_.end(); ++it){
+      (*it)->getPosition();
+    }
 
-		}
-
-		// Reset pointer position to front of array
-		//S_ -= i;
-	}
+    // Reset pointer position to front of array
+    //S_ -= i;
+  }
 }
