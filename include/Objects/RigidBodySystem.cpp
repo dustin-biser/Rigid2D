186657dbd3c3acce77814bc163f6e75a76772093
Dustin Biser <dustinbiser@gmail.com>
2012-02-16 12:29:34 -0500

    Modified curly braces

--- include/Objects/RigidBodySystem.cpp
+++ include/Objects/RigidBodySystem.cpp
@@ -4,10 +4,10 @@
 #include <new>
 
 using namespace std;
-namespace Rigid2D
-{
-  RigidBodySystem::RigidBodySystem( )
-  {
+
+namespace Rigid2D {
+
+  RigidBodySystem::RigidBodySystem( ) {
     time_ = 0.0;
 
     try {
@@ -23,83 +23,69 @@ namespace Rigid2D
 
   }
 
-  RigidBodySystem::~RigidBodySystem()
-  {
+  RigidBodySystem::~RigidBodySystem() {
     delete solver_;
   }
 
-  void RigidBodySystem::updateRigidBodies()
-  {
+  void RigidBodySystem::updateRigidBodies() {
     // Build state array S_ and zero the forceAccumulator for each RigidBody
 
   }
 
-  void RigidBodySystem::addRigidBody(RigidBody *rigidBody)
-  {
+  void RigidBodySystem::addRigidBody(RigidBody *rigidBody) {
     rigidBodies_.insert(rigidBody);
   }
 
-  void RigidBodySystem::addRigidBodies(RigidBody **rigidBodyArray, unsigned int numBodies)
-  {
+  void RigidBodySystem::addRigidBodies(RigidBody **rigidBodyArray, unsigned int numBodies) {
     for (unsigned int i = 0; i < numBodies; ++i)
     {
       rigidBodies_.insert(rigidBodyArray[i]);
     }
   }
 
-  void RigidBodySystem::removeRigidBody(RigidBody *rigidBody)
-  {
+  void RigidBodySystem::removeRigidBody(RigidBody *rigidBody) {
     rigidBodies_.erase(rigidBody);
   }
 
-  void RigidBodySystem::removeRigidBodies(RigidBody **rigidBodyArray, unsigned int numBodies)
-  {
-    for (unsigned int i = 0; i < numBodies; ++i)
-    {
+  void RigidBodySystem::removeRigidBodies(RigidBody **rigidBodyArray, unsigned int numBodies) {
+    for (unsigned int i = 0; i < numBodies; ++i) {
       rigidBodies_.erase(rigidBodyArray[i]);
     }
   }
 
-  void RigidBodySystem::addForce(Force *force)
-  {
+  void RigidBodySystem::addForce(Force *force) {
     forces_.insert(force);
   }
 
-  void RigidBodySystem::addForces(Force **forces, unsigned int numForces)
-  {
-    for (unsigned i = 0; i < numForces; ++i)
-    {
+  void RigidBodySystem::addForces(Force **forces, unsigned int numForces) {
+    for (unsigned i = 0; i < numForces; ++i) {
       forces_.insert(forces[i]);
     }
   }
 
-  void RigidBodySystem::removeForce(Force *force)
-  {
+  void RigidBodySystem::removeForce(Force *force) {
     forces_.erase(force);
   }
 
-  void RigidBodySystem::removeForces(Force **forces, unsigned int numForces)
-  {
-    for (unsigned int i = 0; i < numForces; ++i)
-    {
+  void RigidBodySystem::removeForces(Force **forces, unsigned int numForces) {
+    for (unsigned int i = 0; i < numForces; ++i) {
       forces_.erase(forces[i]);
     }
   }
 
-  void RigidBodySystem::computeStateDeriv(Real t, const Real *S, Real *dSdt)
-  {
+  void RigidBodySystem::computeStateDeriv(Real t, const Real *S, Real *dSdt) {
+
   }
 
-  void RigidBodySystem::buildSystemStateArray()
-  {
+  void RigidBodySystem::buildSystemStateArray() {
     // TODO: Make more robust (don't use 4 as vec dimension)
     Real *S_temp = S_;
 
     unordered_set<RigidBody*>::iterator it;
-    for(it = rigidBodies_.begin(); it != rigidBodies_.end(); ++it)
-    {
+    for(it = rigidBodies_.begin(); it != rigidBodies_.end(); ++it) {
       (*it)->copyState(S_temp);
       S_temp += 4; // 4 is the number of elements copies each time
     }
   }
-}
+
+} // namespace Rigid2D
